2602616ee51db2f2f80ee9867f9c0fa60244f253
ajayrajput1994 <55807531+ajayrajput1994@users.noreply.github.com>
2024-01-10 17:01:51 +0530

    add extra text analyzer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 export default function Navbar(props) {
   return (
-    <nav className="navbar bg-body-tertiary">
+    <nav className="navbar navbar-dark bg-dark ">
     <div className="container-fluid">
       <a href="/" className="navbar-brand">{props.title}</a>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-success" type="submit">Search</button>
       </form>
     </div>
   </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,11 +6,28 @@ export default function TextForm(props) {
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick=()=>{
+        // console.log("handle uppercase function ");
+         let newText=text.toLowerCase();
+         setText(newText);
+     }
+     const handleCopy=()=>{
+        var newtext=document.getElementById("mybox");
+        // console.log("handle uppercase function ");
+         newtext.select();
+         navigator.clipboard.writeText(newtext.value);
+     }
+
+     const handleExtraSpaces=()=>{
+        var newtext=text.split(/[ ]+/);
+        // console.log("handle uppercase function ");
+         setText(newtext.join(" "));
+     }
     const handleonChange=(event)=>{
        // console.log("handle onchange function ");
         setText(event.target.value);
     }
-    const [text,setText]=useState('Enter text here.');
+    const [text,setText]=useState('');
     
   return (
     <>
@@ -20,6 +37,9 @@ export default function TextForm(props) {
     <textarea className="form-control" value={text} id="mybox" onChange={handleonChange} rows="6"></textarea>
     </div>
     <button className="btn btn-outline-primary " type="submit" onClick={handleUPClick}>Convert to Upppercase</button>
+    <button className="btn btn-outline-primary m-2" type="submit" onClick={handleLoClick}>Convert to Lowercase</button>
+    <button className="btn btn-outline-primary m-2" type="submit" onClick={handleCopy}>Copy text</button>
+    <button className="btn btn-outline-primary m-2" type="submit" onClick={handleExtraSpaces}>Clear Extra spaces</button>
     </div>
     <div className="container my-2">
         <h1>Text Summery</h1>
