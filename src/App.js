99732554a8df9dd79ba5f0d78ff7fff553a039c9
ajayrajput1994 <55807531+ajayrajput1994@users.noreply.github.com>
2024-01-13 02:11:49 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import About from './components/About';
+import React,{useState} from 'react';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 function App() {
+  const [modeis,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+     msg:message,
+     type:type 
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1200);
+  }
+  const toggleMode=()=>{
+    if(modeis==="dark"){
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled !","success");
+    }else{
+      setMode('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark mode has been enabled !","success");
+    }
+  }
   return (
     <>
-   <Navbar title="Softwork"/>
+    <Router>
+   <Navbar title="Softwork" mode={modeis} toggleMode={toggleMode} />
+   <Alert alert={alert}/>
     <div className="container">
-      <TextForm headingText="Text to analyze !" />
+    <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} headingText="Text to analyze !" mode={modeis} />} />
+          
+          <Route exact path="/about" element={<About/>} />
+        </Routes>
+      
     </div>
-    
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,23 +5,28 @@ export default function TextForm(props) {
        // console.log("handle uppercase function ");
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Text Convert to UpperCase !","success");
     }
     const handleLoClick=()=>{
         // console.log("handle uppercase function ");
          let newText=text.toLowerCase();
          setText(newText);
+         props.showAlert("Text Convert to LowerCase !","success");
      }
      const handleCopy=()=>{
         var newtext=document.getElementById("mybox");
         // console.log("handle uppercase function ");
          newtext.select();
          navigator.clipboard.writeText(newtext.value);
+         document.getSelection().removeAllRanges();
+         props.showAlert("Text Copy Clipboard !","success");
      }
 
      const handleExtraSpaces=()=>{
         var newtext=text.split(/[ ]+/);
         // console.log("handle uppercase function ");
          setText(newtext.join(" "));
+         props.showAlert("Remove extra spaces !","success");
      }
     const handleonChange=(event)=>{
        // console.log("handle onchange function ");
@@ -31,22 +36,22 @@ export default function TextForm(props) {
     
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{backgroundColor:props.mode==='light'?'dark':'light',color:props.mode==='dark'?'white':'black'}}>
     <h1 className='my-3'>{props.headingText}</h1>
-    <div className="my-3">
-    <textarea className="form-control" value={text} id="mybox" onChange={handleonChange} rows="6"></textarea>
+    <div className="my-3" >
+    <textarea className="form-control" value={text} id="mybox" onChange={handleonChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} rows="6"></textarea>
     </div>
     <button className="btn btn-outline-primary " type="submit" onClick={handleUPClick}>Convert to Upppercase</button>
     <button className="btn btn-outline-primary m-2" type="submit" onClick={handleLoClick}>Convert to Lowercase</button>
     <button className="btn btn-outline-primary m-2" type="submit" onClick={handleCopy}>Copy text</button>
     <button className="btn btn-outline-primary m-2" type="submit" onClick={handleExtraSpaces}>Clear Extra spaces</button>
     </div>
-    <div className="container my-2">
+    <div className="container my-2" style={{backgroundColor:props.mode==='light'?'dark':'light',color:props.mode==='dark'?'white':'black'}}>
         <h1>Text Summery</h1>
-        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{text.split(/\s+/).filter((e)=>{return e.length!==0}).length} Words and {text.length} Characters</p>
         <p>{0.008 * text.split(" ").length} minutes to read </p>
         <h3>Preview :</h3>
-        <div>{text}</div>
+        <div>{text.length>0?text:'Enter text to read preview.'}</div>
     </div>
     </>
   )
